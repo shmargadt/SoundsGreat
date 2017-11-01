a329bd5d8375a1593c4f5a90c8f12d55eef9a5f7
Tamir Ram <tamir.ram@spectory.com>
2017-11-01 22:36:27 +0200

    save

--- src/app/components/user-activity/user-activity.component.ts
+++ src/app/components/user-activity/user-activity.component.ts
@@ -2,23 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { UserActivityActions } from './user-activity.actions';
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http'
 
 @Component({
   selector: 'app-user-activity',
   templateUrl: './user-activity.component.html',
   styleUrls: ['./user-activity.component.scss']
 })
-export class UserActivityComponent  {
-
+export class UserActivityComponent implements OnInit {
+  results: string[];  
   title = 'User Activity';
   @select(state => state.userActivity.activities) readonly activities$: Observable<number>;
 
-  constructor(private actions: UserActivityActions) {}
+  constructor(private actions: UserActivityActions, private http: Http) {}
 
   public pieChartLabels:string[] = ['Mouse events', 'KeyBoard events'];
   public pieChartData:number[] = [300, 100];
   public pieChartType:string = 'pie';
- 
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    console.error('oninit');
+    this.http.get('./keyboardevents.txt').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+      console.log(this.results);
+    });
+  }
   // events
   public chartClicked(e:any):void {
     console.log(e);
